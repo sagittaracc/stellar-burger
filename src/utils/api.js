f7d455e39de1fa32e51a73350df20557e215ddcb
arutyunyan <arutyunyan@bolid.ru>
2023-08-09 19:10:50 +0300

    api refactor

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,21 +2,24 @@ import { STELLAR_BURGER_API } from '../constants/api';
 
 export const request = (url, method, data) => {
     return new Promise((resolve, rejects) => {
-        fetch(`${STELLAR_BURGER_API}${url}`, method === "POST" ? {
-            method: method,
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data)
-        } : null)
-            .then(response => {
-                if (response.ok) {
-                    resolve(response.json());
+        fetch(
+            STELLAR_BURGER_API + url,
+            method === "POST"
+                ? {
+                    method: method,
+                    headers: {"Content-Type": "application/json"},
+                    body: data ? JSON.stringify(data) : null
                 }
-                else {
-                    throw new Error('');
-                }
-            })
+                : null
+        )
+        .then(response => {
+            if (response.ok) {
+                resolve(response.json());
+            }
+            else {
+                throw new Error('Network failure!');
+            }
+        })
     })
 }
 
