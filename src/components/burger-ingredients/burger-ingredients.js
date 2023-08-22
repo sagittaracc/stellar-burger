7a85f99930938d154f3e379d52479d11c7db4f6a
arutyunyan <arutyunyan@bolid.ru>
2023-08-22 14:54:11 +0300

    burger ingredient refactor

--- src/components/burger-ingredients/burger-ingredients.js
+++ src/components/burger-ingredients/burger-ingredients.js
@@ -2,56 +2,46 @@ import React, { useState } from "react";
 import PropTypes from 'prop-types';
 import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
 import { ingredientPropTypes } from "../types/ingredient";
-import Cart from "./cart/cart";
 import Modal from "../modal/modal";
 import IngredientDetails from "./ingredient-details/ingredient-details";
 import useModal from "../../hooks/useModal";
+import IngredientBox from "./ingredient-box/ingredient-box";
 
 
 
 const BurgerIngredients = ({ data }) => {
     const [tab, setTab] = useState('bun');
-    const categories = [
-        { name: "bun", caption: "Булки" },
-        { name: "sauce", caption: "Соусы" },
-        { name: "main", caption: "Начинки" },
-    ];
-
     const [modalShown, openModal, closeModal] = useModal();
     const [current, setCurrent] = useState(null);
+
+    const showIngredient = (ingredient) => {
+        setCurrent(ingredient);
+        openModal();
+    }
+
+    const closeIngredient = () => {
+        closeModal();
+        setCurrent(null);
+    }
 
     return (
         <div className="flex columns text text_type_main-default h-100">
             <h1>Соберите бургер</h1>
 
             <div className="flex">
-                <Tab value="bun" active={tab === 'bun'} onClick={setTab}>
-                    Булки
-                </Tab>
-                <Tab value="sauce" active={tab === 'sauce'} onClick={setTab}>
-                    Соусы
-                </Tab>
-                <Tab value="main" active={tab === 'main'} onClick={setTab}>
-                    Начинки
-                </Tab>
+                <Tab value="bun" active={tab === 'bun'} onClick={setTab}>Булки</Tab>
+                <Tab value="sauce" active={tab === 'sauce'} onClick={setTab}>Соусы</Tab>
+                <Tab value="main" active={tab === 'main'} onClick={setTab}>Начинки</Tab>
             </div>
 
             <div className="custom-scroll full-space overflow-auto">
-                {categories.map(
-                    category =>
-                        <React.Fragment key={category.name}>
-                            <h1 className="text-left pt-10">{category.caption}</h1>
-                            <div className="flex wrap pr-7">
-                                {
-                                    data[category.name].map(ingredient => <Cart onClick={() => {setCurrent(ingredient); openModal();}} key={ingredient._id} ingredient={ingredient}/>)
-                                }
-                            </div>
-                        </React.Fragment>
-                )}
+                <IngredientBox onClick={showIngredient} title="Булки" category="bun" data={data} />
+                <IngredientBox onClick={showIngredient} title="Соусы" category="sauce" data={data} />
+                <IngredientBox onClick={showIngredient} title="Начинки" category="main" data={data} />
             </div>
             {
                 modalShown && current &&
-                <Modal header="Детали ингредиента" onClose={() => {closeModal(); setCurrent(null)}}>
+                <Modal header="Детали ингредиента" onClose={closeIngredient}>
                     <IngredientDetails ingredient={current} />
                 </Modal>
             }
---
+++ src/components/burger-ingredients/ingredient-box/ingredient-box.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Cart from "../cart/cart";
+import PropTypes from 'prop-types';
+import { ingredientPropTypes } from "../../types/ingredient";
+
+const IngredientBox = ({onClick, title, data, category}) => {
+    return (
+        <React.Fragment>
+            <h1 className="text-left pt-10">{title}</h1>
+            <div className="flex wrap pr-7">
+                {
+                    data[category].map(ingredient => <Cart onClick={() => onClick(ingredient)} key={ingredient._id} ingredient={ingredient} />)
+                }
+            </div>
+        </React.Fragment>
+    )
+}
+
+IngredientBox.propTypes = {
+    onClick: PropTypes.func,
+    title: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+    data: PropTypes.shape({
+        bun: PropTypes.arrayOf(ingredientPropTypes),
+        sauce: PropTypes.arrayOf(ingredientPropTypes),
+        main: PropTypes.arrayOf(ingredientPropTypes),
+    })
+};
+
+export default IngredientBox;
