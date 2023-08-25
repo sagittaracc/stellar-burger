6203619750f1b3eb4ab18ced29035eac75a2ab47
arutyunyan <arutyunyan@bolid.ru>
2023-08-25 15:11:35 +0300

    Сделал удаление ингредиента из конструктора

--- src/components/burger-constructor/cart/cart.js
+++ src/components/burger-constructor/cart/cart.js
@@ -1,8 +1,16 @@
 import PropTypes from 'prop-types';
 import { ingredientPropTypes } from '../../types/ingredient';
 import { DragIcon, ConstructorElement } from '@ya.praktikum/react-developer-burger-ui-components';
+import { useDispatch } from 'react-redux';
+import { delIngredient } from '../../../services/constructor/actions';
 
 const Cart = ({ ingredient }) => {
+    const dispatch = useDispatch();
+
+    const handleClose = () => {
+        dispatch(delIngredient(ingredient.id));
+    }
+
     return (
         <div className="flex align-center pb-4">
             <span className="mr-4"><DragIcon type="primary" /></span>
@@ -10,6 +18,7 @@ const Cart = ({ ingredient }) => {
                 text={ingredient.name}
                 price={ingredient.price}
                 thumbnail={ingredient.image}
+                handleClose={handleClose}
             />
         </div>
     );
