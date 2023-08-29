fe3480ffb6ac710230014e5dde8cac8a676886f4
Yuriy Arutyunyan <sagittaracc@gmail.com>
2023-08-29 17:58:09 +0300

    вложенные маршруты

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,6 +9,7 @@ import Ingredient from '../../pages/ingredient';
 import styles from './app.module.css';
 import AppHeader from '../app-header/app-header';
 import { Link } from 'react-router-dom';
+import Credentials from '../../pages/profile/credentials';
 
 function App() {
     return (
@@ -25,7 +26,10 @@ function App() {
                     <Route path='/register' element={<Register />} />
                     <Route path='/forgot-password' element={<ForgotPassword />} />
                     <Route path='/reset-password' element={<ResetPassword />} />
-                    <Route path='/profile' element={<Profile />} />
+                    <Route path='/profile/*' element={<Profile />}>
+                        <Route path='' element={<Credentials />} />
+                        <Route path='orders' element={<p>Orders</p>} />
+                    </Route>
                     <Route path='/ingredients/:id' element={<Ingredient />} />
                 </Routes>
             </main>
--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -1,20 +1,20 @@
 import styles from './profile.module.css';
 import NavLink from '../../components/nav-link/nav-link';
-import Credentials from './credentials';
+import { Outlet } from 'react-router-dom';
 
 const Profile = () => {
     return (
         <div className={`${styles.profile} flex`}>
             <div className="col">
                 <ul className={`${styles.nav} m-0 p-0 pb-20`}>
-                    <li><NavLink to="/profile" exact text="Профиль" size="medium" /></li>
-                    <li><NavLink to="/profile/orders" exact text="История заказов" size="medium" /></li>
-                    <li><NavLink to="/profile/logout" text="Выход" size="medium"/></li>
+                    <li><NavLink to="" exact text="Профиль" size="medium" /></li>
+                    <li><NavLink to="orders" exact text="История заказов" size="medium" /></li>
+                    <li><NavLink to="logout" text="Выход" size="medium"/></li>
                 </ul>
                 <p className="text text_color_inactive">В этом разделе вы можете изменить свои персональные данные</p>
             </div>
             <div className="col">
-                <Credentials />
+                <Outlet />
             </div>
             <div className="col"></div>
         </div>
