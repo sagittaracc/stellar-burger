d923593164b69a2688dc1b32974d81465cd5e969
Yuriy Arutyunyan <sagittaracc@gmail.com>
2023-08-31 00:29:31 +0300

    done refactor forms

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,15 +1,15 @@
-import { Button, Input } from "@ya.praktikum/react-developer-burger-ui-components";
 import { Link } from 'react-router-dom';
 import styles from './form.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { register } from '../services/register/actions';
 import Alert from '../components/alert/alert';
 import useForm from '../hooks/useForm';
-import { getFormErrorSelector, isFormRequestSelector } from "../services/form/selectors";
+import { getFormErrorSelector } from "../services/form/selectors";
+import FormInput from "../components/form/form-input";
+import SubmitButton from "../components/form/submit-button";
 
 const Register = () => {
     const dispatch = useDispatch();
-    const isRequest = useSelector(isFormRequestSelector);
     const error = useSelector(getFormErrorSelector);
 
     const form = useForm();
@@ -24,22 +24,10 @@ const Register = () => {
             { error && <Alert message={error} type="danger" /> }
             <form onSubmit={handleSubmit(onSubmit)} className={`${styles.form}`}>
                 <h1>Регистрация</h1>
-                <Input
-                    {...field('name')} disabled={isRequest} type="text" placeholder="Имя"
-                    extraClass="mt-6" />
-                <Input
-                    {...field('email')}
-                    disabled={isRequest} type="text" placeholder="E-mail"
-                    extraClass="mt-6" />
-                <Input
-                    {...field('password')}
-                    disabled={isRequest} type="password" placeholder="Пароль"
-                    extraClass="mt-6" icon="HideIcon" />
-                <Button
-                    disabled={isRequest} htmlType="submit" type="primary"
-                    size="medium" extraClass="mt-6">
-                    Зарегистрироваться
-                </Button>
+                <FormInput {...field('name')} type="text" placeholder="Имя"  />
+                <FormInput {...field('email')} type="email" placeholder="E-mail" />
+                <FormInput {...field('password')} type="password" placeholder="Пароль" icon="HideIcon" />
+                <SubmitButton type="primary">Зарегистрироваться</SubmitButton>
 
                 <p className="text text_type_main-default mt-20">
                     Уже зарегистрированы? <Link to="/login">Войти</Link></p>
