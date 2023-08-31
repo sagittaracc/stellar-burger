b641cc077b26ea68ce0f01b695f8a13565c6e1d0
Yuriy Arutyunyan <sagittaracc@gmail.com>
2023-08-31 15:55:43 +0300

    Сохраняю access token in cookies

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,15 +1,19 @@
+import Cookies from 'js-cookie';
+
+const ACCESS_TOKEN_LIFETIME = 20; // в минутах
+
 export const saveTokens = ({accessToken, refreshToken}) => {
-    localStorage.setItem('accessToken', accessToken);
+    Cookies.set('accessToken', accessToken, {expires: ACCESS_TOKEN_LIFETIME / 1440});
     localStorage.setItem('refreshToken', refreshToken);
 }
 
 export const removeTokens = () => {
-    localStorage.removeItem('accessToken');
+    Cookies.remove('accessToken');
     localStorage.removeItem('refreshToken');
 }
 
 export const getAccessToken = () => {
-    return localStorage.getItem('accessToken');
+    return Cookies.get('accessToken');
 }
 
 export const getRefreshToken = () => {
