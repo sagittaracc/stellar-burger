6a908aba514363bbbe796e9b39a7a341b2b843a5
Yuriy Arutyunyan <sagittaracc@gmail.com>
2023-09-01 23:32:05 +0300

    мигала форма редактирования профиля

--- src/services/auth/actions.js
+++ src/services/auth/actions.js
@@ -1,10 +1,11 @@
 import { secureGet, securePatch } from "../../utils/api";
+import { FORM_FAIL, FORM_REQUEST, FORM_SUCCESS } from "../form/actions";
 
 export const AUTH_REQUEST = 'AUTH/REQUEST';
 export const SET_AUTH = 'AUTH/SET';
 export const UNSET_AUTH = 'AUTH/UNSET';
 
-export const setUser = ({email, name}) => {
+export const setUser = ({ email, name }) => {
     return {
         type: SET_AUTH,
         payload: {
@@ -33,13 +34,15 @@ export const getUser = () => (dispatch) => {
 }
 
 export const updUser = (data) => (dispatch) => {
-    dispatch({ type: AUTH_REQUEST });
+    dispatch({ type: FORM_REQUEST });
 
     securePatch('/auth/user', data)
         .then(response => {
             dispatch(setUser(response.user));
+            dispatch({ type: FORM_SUCCESS });
         })
         .catch(error => {
             dispatch({ type: UNSET_AUTH });
+            dispatch({ type: FORM_FAIL, payload: error.message });
         })
 }
