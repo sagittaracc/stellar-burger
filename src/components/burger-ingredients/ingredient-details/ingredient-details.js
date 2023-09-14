71ea2cdf4fa543303221811c02dea94a6bf9e9ab
arutyunyan <arutyunyan@bolid.ru>
2023-09-14 17:12:20 +0300

    code styles

--- src/components/burger-ingredients/ingredient-details/ingredient-details.js
+++ src/components/burger-ingredients/ingredient-details/ingredient-details.js
@@ -4,20 +4,25 @@ const IngredientDetails = ({ ingredient }) => {
     return (
         <div className="text text-center mt-10">
             <div><img src={ingredient.image_large} /></div>
+
             <div className="mt-4 text_type_main-medium">{ingredient.name}</div>
+
             <div className="flex ml-10 mr-10 mt-8 text_type_main-default text_color_inactive">
                 <div className="col">
                     <p>Калории, ккал</p>
                     <p className="text_type_digits-default">{ingredient.calories}</p>
                 </div>
+
                 <div className="col">
                     <p>Белки, г</p>
                     <p className="text_type_digits-default">{ingredient.proteins}</p>
                 </div>
+
                 <div className="col">
                     <p>Жиры, г</p>
                     <p className="text_type_digits-default">{ingredient.fat}</p>
                 </div>
+
                 <div className="col">
                     <p>Углеводы, г</p>
                     <p className="text_type_digits-default">{ingredient.carbohydrates}</p>
