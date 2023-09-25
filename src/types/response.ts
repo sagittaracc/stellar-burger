b0ae62cab2ebd84e03a821f88e61039429cd6b4d
Yuriy Arutyunyan <sagittaracc@gmail.com>
2023-09-25 21:36:46 +0300

    refactor

--- src/types/response.ts
+++ src/types/response.ts
@@ -4,6 +4,14 @@ export type TSuccess = {
     success: true;
 };
 
+export type TError = {
+    success: false;
+};
+
 export type TIngredientResponse = TSuccess & {
     data: Array<TIngredient>;
+};
+
+export type TErrorResponse = TError & {
+    message: string;
 };
