d8e07352c58d84612556bf4ca7c88cb6cbf9761a
arutyunyan <arutyunyan@bolid.ru>
2023-10-05 13:38:55 +0300

    закончил с версткой страницы лента заказов

--- src/components/feed/orders/orders.tsx
+++ src/components/feed/orders/orders.tsx
@@ -5,7 +5,7 @@ import { orders } from '../../../stubs/orders';
 const Orders: FC = () => {
     return (
         <div className="flex columns text text_type_main-default h-100">
-            <h1>Лента заказов</h1>
+            <h1 className='mb-3'>Лента заказов</h1>
             <div className="custom-scroll full-space overflow-auto">
                 {orders.map(order => <CardOrder data={order} />)}
             </div>
--- src/components/feed/stats/stats.tsx
+++ src/components/feed/stats/stats.tsx
@@ -1,8 +1,41 @@
 import { FC } from 'react';
+import styles from './stats.module.css';
 
 const Stats: FC = () => {
     return (
-        <>Stats</>
+        <div className='mt-20 ml-15'>
+            <div className='flex'>
+                <div className="col">
+                    <h3 className='text text_type_main-medium mb-6'>Готов:</h3>
+                    <div className="flex">
+                        <div className="col">
+                            <p className={`${styles.orderNumber} text text_type_digits-default`}>22097</p>
+                            <p className={`${styles.orderNumber} text text_type_digits-default`}>22097</p>
+                            <p className={`${styles.orderNumber} text text_type_digits-default`}>22097</p>
+                            <p className={`${styles.orderNumber} text text_type_digits-default`}>22097</p>
+                            <p className={`${styles.orderNumber} text text_type_digits-default`}>22097</p>
+                        </div>
+                        <div className="col">
+                            <p className={`${styles.orderNumber} text text_type_digits-default`}>22096</p>
+                            <p className={`${styles.orderNumber} text text_type_digits-default`}>22096</p>
+                            <p className={`${styles.orderNumber} text text_type_digits-default`}>22096</p>
+                            <p className={`${styles.orderNumber} text text_type_digits-default`}>22096</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <h3 className='text text_type_main-medium mb-6'>В работе:</h3>
+                </div>
+            </div>
+            <div>
+                <h3 className='text text_type_main-medium mt-15'>Выполнено за всё время:</h3>
+                <p className='text text_type_digits-large'>21723</p>
+            </div>
+            <div>
+                <h3 className='text text_type_main-medium mt-15'>Выполнено за сегодня:</h3>
+                <p className='text text_type_digits-large'>60</p>
+            </div>
+        </div>
     );
 }
 
