6c1c6cc978d5de2e3745a1c6b58d5bf8015086d6
arutyunyan <arutyunyan@bolid.ru>
2023-10-06 13:39:58 +0300

    refactor

--- src/types/feed.ts
+++ src/types/feed.ts
@@ -1,8 +1,6 @@
 import { TIngredient } from "./ingredient";
 import { TOrder } from "./order";
 
-export type TIngredientList = Array<string>;
-
 export type TFeed = {
     orders: Array<TOrder>,
     total: number,
@@ -10,15 +8,7 @@ export type TFeed = {
 };
 
 export type TCardOrderDetailsComponent = {
-    data: {
-        _id: string,
-        ingredients: TIngredientList,
-        status: string,
-        name: string,
-        createdAt: string,
-        updatedAt: string,
-        number: number
-    },
+    data: TOrder,
     cost: number,
     list: Array<TIngredient>,
 };
