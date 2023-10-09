f70c90db44a9d6cf2e463aed7a60d0e714df0eda
arutyunyan <arutyunyan@bolid.ru>
2023-10-09 13:53:17 +0300

    refactor

--- src/components/order/order-details/order-details.tsx
+++ src/components/order/order-details/order-details.tsx
@@ -6,15 +6,7 @@ import { TOrderComponent } from "../../../types/order";
 import OrderStatus from "../order-status/order-status";
 import withCost from "../../../hocs/with-cost";
 
-const OrderDetails: FC<TOrderComponent> = ({ order, ingredients, cost }) => {
-    const OrderWithCost = withCost(Order);
-
-    return (
-        <OrderWithCost order={order} ingredients={ingredients} cost={cost} />
-    );
-};
-
-const Order: FC<TOrderComponent> = ({ order, ingredients, cost }) => {
+const OrderDetails: FC<TOrderComponent> = withCost(({ order, ingredients, cost }) => {
     return (
         <div className="ml-10 mr-10">
             <p className='text text_type_main-medium mb-1'>{order.name}</p>
@@ -39,6 +31,6 @@ const Order: FC<TOrderComponent> = ({ order, ingredients, cost }) => {
             </div>
         </div>
     );
-}
+})
 
 export default OrderDetails;
