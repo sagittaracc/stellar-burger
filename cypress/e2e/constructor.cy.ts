cc297e3fc3dd9dd54078fcf601b78ff7484b1fe4
Yuriy Arutyunyan <sagittaracc@gmail.com>
2023-10-12 23:28:01 +0300

    закончил с тестом заказа буду проверять

--- cypress/e2e/constructor.cy.ts
+++ cypress/e2e/constructor.cy.ts
@@ -5,6 +5,7 @@ describe('test constructor', () => {
   beforeEach(() => {
     cy.intercept("GET", `${STELLAR_BURGER_API}/auth/user`, { fixture: "user.json" });
     cy.intercept("POST", `${STELLAR_BURGER_API}/orders`, { fixture: "order.json" }).as("postOrder");
+    cy.intercept("GET", `${STELLAR_BURGER_API}/ingredients`).as("getIngredients");
 
     saveTokens({
       accessToken: "test-accessToken",
@@ -14,17 +15,23 @@ describe('test constructor', () => {
 
   it('should create an order', () => {
     cy.visit("https://sagittaracc.github.io/stellar-burger/");
+    cy.wait("@getIngredients");
 
     cy.contains("Краторная булка").as("bun");
     cy.contains("Соус традиционный").as("sauce");
     cy.contains("Говяжий метеорит").as("main");
-    cy.contains("[id^=constructor]").as("constructor");
+    cy.get("[id^=constructor]").as("constructor");
 
-    // cy.get("@bun").trigger("dragstart");
-    // cy.get("@constructor").trigger("drop");
-    // cy.get("@sauce").trigger("dragstart");
-    // cy.get("@constructor").trigger("drop");
-    // cy.get("@main").trigger("dragstart");
-    // cy.get("@constructor").trigger("drop");
+    cy.get("@bun").trigger("dragstart");
+    cy.get("@constructor").trigger("drop");
+    cy.get("@sauce").trigger("dragstart");
+    cy.get("@constructor").trigger("drop");
+    cy.get("@main").trigger("dragstart");
+    cy.get("@constructor").trigger("drop");
+
+    cy.get("button").contains("Оформить заказ").as("submit");
+    cy.get("@submit").click();
+
+    cy.get("[data-testid=order-number]").contains("777").should("exist");
   })
 })
