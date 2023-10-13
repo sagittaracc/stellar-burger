86380cfeb8f2c2f593508f8693a2947351c62007
arutyunyan <arutyunyan@bolid.ru>
2023-10-13 09:54:30 +0300

    auth -> ts

--- src/services/auth/selectors.js
+++
@@ -1,5 +0,0 @@
-export const getAuthRequestSelector = (store) => store.auth.authRequest;
-export const getAuthCheckedSelector = (store) => store.auth.authChecked;
-export const getAuthSuccessSelector = (store) => store.auth.authSuccess;
-export const getEmailSelector = (store) => store.auth.email;
-export const getNameSelector = (store) => store.auth.name;
